4cb79d2b84926f4610b00b7e9801d6b8582f4cd8
U733584 <savan.rangegowda@lhind.dlh.de>
2020-04-26 16:56:58 +0200

    Remove venv folder

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, jsonify
+from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import cv2
-import keras
 import numpy as np
 from keras.models import load_model
 from keras import backend as K
@@ -15,12 +14,15 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
-	return '.' in filename and \
-		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	"""allowed files"""
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+	"""This method gives the input to the ML Model and gets the Predictions"""
 	if request.method == 'POST':
 		# check if the post request has the file part
 		if 'file' not in request.files:
@@ -33,77 +35,77 @@ def upload_file():
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			image = cv2.imread(os.path.dirname(os.path.realpath(__file__))+"/uploads/"+filename)
-			color_result = getDominantColor(image)
-			result = catOrDog(image)
-			redirect(url_for('upload_file',filename=filename))
+			image = cv2.imread(os.path.dirname(os.path.realpath(__file__)) + "/uploads/" + filename)
+			color_result = get_dominant_color(image)
+			result = cat_or_dog(image)
+			redirect(url_for('upload_file', filename=filename))
 			return '''
-			<!doctype html>
-			<title>Savan's AI Results</title>
-			<style>
-			input[type=file], select {
-		  	width: 100%;
-		  	padding: 12px 20px;
-		  	margin: 8px 0;
-		  	display: inline-block;
-		  	border: 1px solid #ccc;
-		  	border-radius: 4px;
-		  	box-sizing: border-box;
-			}
-			</style>
-            <div style="text-align:center;border:5px dashed white;background-color:#09117C;">
-                <p></p>
-                <p style="color:white;font-size:40px;">Savan's Cat and Dog Machine Learning Model... Enjoy! :)</p>
-                <p></p>
-            </div>
-            <div style="text-align:center;border:5px dashed black;background-color:white;">
-                <p style="color:black;font-size:20px"> Upload another pic of a Cat or a Dog </p>
-			        <form method=post enctype=multipart/form-data>
-			        <input type=file name=file>
-			        <input type=submit value=Upload>
-			        </form>
-            </div>
-            <div style="text-align:center;border:5px dashed #D9D61F;background-color:white;">
-			    <p style="color:green;font-size:25px">The image contains a - '''+result+'''</p>
-			    <p style="color:green;font-size:25px">The dominant color in the image is - '''+color_result+'''</p>
-            </div>
-			'''
+				<!DOCTYPE html>
+					<title> Savan's AI Results </title>
+					<style>
+						input[type=file], select {
+							width: 100%;
+							padding: 12px 20px;
+							margin: 8px 0;
+							display: inline-block;
+							border: 1px solid #ccc;
+							border-radius: 4px;
+							box-sizing: border-box;
+						}
+					</style>
+					<div style="text-align:center;border:5px dashed white;background-color:#09117C;">
+						<p></p>
+						<p style="color:white;font-size:40px;">Savan's Cat and Dog Machine Learning Model... Enjoy! :)</p>
+						<p></p>
+					</div>
+					<div style="text-align:center;border:5px dashed black;background-color:white;">
+						<p style="color:black;font-size:20px"> Upload another pic of a Cat or a Dog </p>
+						<form method=post enctype=multipart/form-data>
+							<input type=file name=file>
+							<input type=submit value=Upload>
+						</form>
+					</div>
+					<div style="text-align:center;border:5px dashed #D9D61F;background-color:white;">
+						<p style="color:green;font-size:25px">The image contains a - '''+result+'''</p>
+						<p style="color:green;font-size:25px">The dominant color in the image is - '''+color_result+'''</p>
+					</div>
+				'''
 	return '''
-	<!doctype html>
-	<title>Savan's AI Homepage</title>
-	<style>
-	input[type=file], select {
-  	width: 100%;
-  	padding: 12px 20px;
-  	margin: 8px 0;
-  	display: inline-block;
-  	border: 1px solid #ccc;
-  	border-radius: 4px;
-  	box-sizing: border-box;
-	}
-	</style>
-    <div style="text-align:center;border:5px dashed white;background-color:#09117C;">
-        <p></p>
-        <p style="color:white;font-size:40px;">Savan's Cat and Dog Machine Learning Model... Enjoy! :)</p>
-        <p></p>
-    </div>
+		<!doctype html>
+			<title>Savan's AI Homepage</title>
+			<style>
+				input[type=file], select {
+					width: 100%;
+					padding: 12px 20px;
+					margin: 8px 0;
+					display: inline-block;
+					border: 1px solid #ccc;
+					border-radius: 4px;
+					box-sizing: border-box;
+				}
+			</style>
+			<div style="text-align:center;border:5px dashed white;background-color:#09117C;">
+				<p></p>
+				<p style="color:white;font-size:40px;">Savan's Cat and Dog Machine Learning Model... Enjoy! :)</p>
+				<p></p>
+			</div>
+			<div style="text-align:center;border:5px dashed black;background-color:white;">
+				<p style="color:black;font-size:20px"> Upload any pic of a Cat or a Dog </p>
+				<form method=post enctype=multipart/form-data>
+					<input type=file name=file>
+					<input type=submit value=Upload>
+				</form>
+				<p></p>
+			</div>
+		'''
 
-    <div style="text-align:center;border:5px dashed black;background-color:white;">
-        <p style="color:black;font-size:20px"> Upload any pic of a Cat or a Dog </p>
-            <form method=post enctype=multipart/form-data>
-            <input type=file name=file>
-            <input type=submit value=Upload>
-            </form>
-		<p></p>
-    </div>
-	'''
 
-def catOrDog(image):
-	'''Determines if the image contains a cat or dog'''
+def cat_or_dog(image):
+	""" Determines if the image contains a cat or dog"""
 	classifier = load_model('./models/cats_vs_dogs_V1.h5')
-	image = cv2.resize(image, (150,150), interpolation = cv2.INTER_AREA)
-	image = image.reshape(1,150,150,3)
-	res = str(classifier.predict_classes(image, 1, verbose = 0)[0][0])
+	image = cv2.resize(image, (150, 150), interpolation=cv2.INTER_AREA)
+	image = image.reshape(1, 150, 150, 3)
+	res = str(classifier.predict_classes(image, 1, verbose=0)[0][0])
 	print(res)
 	print(type(res))
 	if res == "0":
@@ -113,13 +115,15 @@ def catOrDog(image):
 	K.clear_session()
 	return res
 
-def getDominantColor(image):
-	'''returns the dominate color among Blue, Green and Reds in the image '''
-	B, G, R = cv2.split(image)
-	B, G, R = np.sum(B), np.sum(G), np.sum(R)
-	color_sums = [B,G,R]
+
+def get_dominant_color(image):
+	"""returns the dominate color among Blue, Green and Reds in the image"""
+	blue, green, red = cv2.split(image)
+	blue, green, red = np.sum(blue), np.sum(green), np.sum(red)
+	color_sums = [blue, green, red]az
 	color_values = {"0": "Blue", "1":"Green", "2": "Red"}
 	return color_values[str(np.argmax(color_sums))]
 
+
 if __name__ == "__main__":
-	app.run(host= '0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=80)
